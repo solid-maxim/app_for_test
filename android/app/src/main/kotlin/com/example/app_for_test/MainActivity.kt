9c4099bd153e49f5308f33f1d4f6ac27f2cf475b
solid-maxim <maxim.ustych@solid.software>
2023-02-05 17:24:36 +0200

    wip

--- android/app/src/main/kotlin/com/example/app_for_test/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.app_for_test
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ core_eggy/android/app/src/main/kotlin/com/example/core_eggy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.core_eggy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
